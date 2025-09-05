991eb3c144da8f623016385371f33b0ee3da30a7
panashe-laurent <panashe.nyangaire@omnicontact.biz>
2025-09-05 10:04:21 +0200

    fixtures model

---
+++ models/fixtures.js
@@ -0,0 +1,41 @@
+import { DataTypes } from 'sequelize';
+
+const Fixtures = (sequelize) => {
+    const Fixtures = sequelize.define(
+        'fixtures',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                unique: true,
+                primaryKey: true,
+                allowNull: false,
+            },
+
+            fixture_code: {
+                type: DataTypes.UUID,
+                allowNull: false,
+            },
+
+            day: {
+                type: DataTypes.DATE,
+                allowNull: true, 
+            },
+
+            time: {
+                type: DataTypes.TIME,
+                allowNull: true,
+            },
+
+        },
+        {
+            tableName: 'fixtures',
+            timestamps: true,
+            underscored: true,
+        }
+    );
+
+    return Fixtures;
+};
+
+export default Fixtures;
